7c1f581287257584d89a025beba99a8d37772b96
Victor <1149833549@qq.com>
2022-07-07 17:20:08 +0800

    209 暴力搜索

--- 代码随想录/数组/3. 滑动窗口/209.长度最小的子数组.cpp
+++ 代码随想录/数组/3. 滑动窗口/209.长度最小的子数组.cpp
@@ -3,6 +3,22 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-
+        int minlen = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            int sum = 0;
+            for (int j = i; j < nums.size(); j++) {
+                sum += nums[j];
+                if (sum >= target) {
+                    if (minlen == 0) {
+                        minlen = j - i + 1;
+                    }
+                    else {
+                        minlen = minlen < (j - i + 1) ? minlen : j - i + 1;
+                    }
+                    break;
+                }
+            }
+        }
+        return minlen;
     }
 };
