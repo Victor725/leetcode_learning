ed097d45278e29100552b6d1f22011820a19929d
Victor <1149833549@qq.com>
2022-07-07 18:11:53 +0800

    76 done

---
+++ 代码随想录/数组/3. 滑动窗口/76. 最小覆盖子串.cpp
@@ -0,0 +1,47 @@
+#include"global.h"
+
+/*
+窗口：包含t中所有字母的串
+移动起始：已经包含，即可右移
+如何判断是否包含所有字母了
+*/
+class Solution {
+public:
+    unordered_map <char, int> ori, cnt;
+
+    bool check() {
+        for (const auto& p : ori) {
+            if (cnt[p.first] < p.second) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    string minWindow(string s, string t) {
+        for (const auto& c : t) {
+            ++ori[c];
+        }
+
+        int l = 0, r = -1;
+        int len = INT_MAX, ansL = -1, ansR = -1;
+
+        while (r < int(s.size())) {
+            if (ori.find(s[++r]) != ori.end()) {
+                ++cnt[s[r]];
+            }
+            while (check() && l <= r) {
+                if (r - l + 1 < len) {
+                    len = r - l + 1;
+                    ansL = l;
+                }
+                if (ori.find(s[l]) != ori.end()) {
+                    --cnt[s[l]];
+                }
+                ++l;
+            }
+        }
+
+        return ansL == -1 ? string() : s.substr(ansL, len);
+    }
+};
